dd1f75e34f54588901a9a47add5dba9740b5fd7c
rajsudeep <sudrocks@gmail.com>
2019-09-09 20:59:47 +0000

    Add task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * _find_listint_loop - finds the start of the loop
+ * @head: pointer to the list with a loop
+ * @marker: marked node where loop was certified
+ *
+ * Return: Address of the node where the loop starts
+ */
+listint_t *_find_listint_loop(listint_t *head, listint_t *marker)
+{
+        listint_t *curr = head;
+
+        while (curr != marker)
+        {
+                curr = curr->next;
+                marker = marker->next;
+        }
+        return (curr);
+}
+
+/**
+ * find_listint_loop - detects for a loop in a list
+ * @head: pointer to a list of numbers
+ *
+ * Return: Address of the node where loop starts
+ * NULL if no loop exists
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+        listint_t *marker = head, *next = head;
+
+        while (marker != NULL && next != NULL && next->next != NULL)
+        {
+                marker = marker->next;
+                next = next->next->next;
+                if (marker == next)
+                        return (_find_listint_loop(head, marker));
+        }
+        return (NULL);
+}
+
+/**
+ * check_cycle - check if singly linked list has a cycle
+ * @list: linked list to check in
+ *
+ * Return: 0 if there is none, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	if (find_listint_loop(list) == NULL)
+		return (0);
+	else
+		return (1);
+}
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,20 @@
+#ifndef _LISTS_
+#define _LISTS_
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+int check_cycle(listint_t *list);
+
+#endif
