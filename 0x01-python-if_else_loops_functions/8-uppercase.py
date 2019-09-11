0cd201a00f1fb8a986c690d517cc5b4d2bb78d1f
rajsudeep <sudrocks@gmail.com>
2019-09-11 01:50:46 +0000

    Remove func description

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Converts lower chars in a str to upper and prints."""
     for c in str:
         letternum = ord(c)
         if (letternum >= 97 and letternum <= 122):
