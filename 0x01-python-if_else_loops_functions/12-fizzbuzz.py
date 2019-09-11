e5ad1a9137b472bb6aec9bc6b19e0adbc2330803
rajsudeep <sudrocks@gmail.com>
2019-09-11 03:56:58 +0000

    Handle Pepper error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,4 +7,4 @@ def fizzbuzz():
             print("Buzz", end='')
         if (i % 3) != 0 and (i % 5) != 0:
             print("{:d}".format(i), end='')
-        print(" ", end= '')
+        print(" ", end='')
