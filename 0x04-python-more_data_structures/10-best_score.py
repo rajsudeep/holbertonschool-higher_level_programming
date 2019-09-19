7483db423feebef232c126f3dd4ca5300cb2598f
rajsudeep <sudrocks@gmail.com>
2019-09-19 19:33:21 +0000

    Use == when comparing to value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary is {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     max = list(a_dictionary.keys())[0]
     for key in a_dictionary.keys():
