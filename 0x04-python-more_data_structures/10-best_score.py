627575925219d37591e5e14b3800edc72d07a2cc
rajsudeep <sudrocks@gmail.com>
2019-09-19 19:32:31 +0000

    Use is when comparing to None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or a_dictionary == {}:
+    if a_dictionary is None or a_dictionary is {}:
         return None
     max = list(a_dictionary.keys())[0]
     for key in a_dictionary.keys():
