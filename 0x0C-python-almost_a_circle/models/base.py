2074896418eafc925ff3289ea4285dc2c6fc90a9
rajsudeep <sudrocks@gmail.com>
2019-10-20 20:13:30 +0000

    Initial set up of base and rectangle classes

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Class Base -
+manages the id attribute
+
+"""
+
+
+class Base:
+    """ creates an identifier for an instance """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Class BaseTest -
+for testing models/base.py
+
+"""
+
+
+import unittest
+from models.base import Base
+
+class BaseTest(unittest.TestCase):
+    """ unit tests for the base class """
+    def test_incrementation:
+        """ sees if object id increments """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+    def test_inputs:
+        """ checks for various valid inputs """
+        b_zero = Base()
+        self.assertEqual(b1.zero, 1)
+
+        b_int = Base(12)
+        self.assertEqual(b_int.id, 12)
+
+        b_neg_int = Base(-12)
+        self.assertEqual(b_neg_int.id, -12)
+
+        b_str = Base("string")
+        self.assertEqual(b_str.id, "string")
+
+        b_list = Base([1, 2, 3])
+        self.assertEqual(b_list.id, [1, 2, 3])
+
+if __name__ == '__main__':
+    unittest.main()
