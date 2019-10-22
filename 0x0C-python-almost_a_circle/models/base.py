1b109a1290cbd1bdaa87b8bc7a38cf4063a98038
rajsudeep <sudrocks@gmail.com>
2019-10-22 07:03:38 +0000

    Add json fts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@ Class Base -
 manages the id attribute
 
 """
+from json import dumps, loads
+from os import path
 
 
 class Base:
@@ -11,8 +13,49 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ inits an id """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns JSON representation of dictionaries """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save objects to a file """
+        if list_objs is not None:
+            list_objs = [obj.to_dictionary() for obj in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="UTF-8") as f:
+            f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation """
+        return loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set """
+        obj = None
+        if cls.__name__ == "Rectangle":
+            obj = cls(2, 3)
+        elif cls.__name__ == "Square":
+            obj = cls(2)
+        obj.update(**dictionary)
+        return obj
+
+    @classmethod
+    def load_from_file(cls):
+        """ loads a file and returns a list of instances from it """
+        if not path.isfile("{}.json".format(cls.__name__)):
+            return []
+        with open("{}.json".format(cls.__name__), "r", encoding="utf-8") as f:
+                return [cls.create(**obj) for obj in cls.from_json_string(
+                    f.read())]
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,11 +8,11 @@ for testing models/base.py
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 class BaseTest(unittest.TestCase):
     """ unit tests for the base class """
     def test_incrementation(self):
-        """ sees if object id increments """
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
@@ -22,18 +22,38 @@ class BaseTest(unittest.TestCase):
         b3 = Base()
         self.assertEqual(b3.id, 3)
     def test_inputs(self):
-        """ checks for various valid inputs """
         b_int = Base(12)
         self.assertEqual(b_int.id, 12)
 
         b_neg_int = Base(-12)
         self.assertEqual(b_neg_int.id, -12)
+    def test_json_conversions(self):
+        r = Base.to_json_string(None)
+        self.assertEqual(r, "[]")
 
-        b_str = Base("string")
-        self.assertEqual(b_str.id, "string")
+        r = Base.to_json_string([])
+        self.assertEqual(r, "[]")
 
-        b_list = Base([1, 2, 3])
-        self.assertEqual(b_list.id, [1, 2, 3])
+        r = Rectangle(10, 7, 2, 8)
+        dict = r.to_dictionary()
+        json_dict = Base.to_json_string([dict])
+        result = [{'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}]
+        self.assertEqual(len(json_dict), len(str(result)))
 
+        input = '[{"x": 1, "y": 2, "id": 3}, \
+        {"x": 2, "y": 2, "id": 2}]'
+        output = [{'x': 1, 'y': 2, 'id': 3},
+             {'x': 2, 'y': 2, 'id': 2}]
+        self.assertEqual(Base.from_json_string(input), output)
+    def test_save_to_file(self):
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), "[]")
+    def test_create(self):
+        r = Rectangle(3, 5, 1, 1, 100)
+        r_dict = r.to_dictionary()
+        r_x = Rectangle.create(**r_dict)
+        self.assertEqual(r is r_x, False)
+    
 if __name__ == '__main__':
     unittest.main()
