7e713644527c47405e45966c742c7025ebf12fb9
rajsudeep <sudrocks@gmail.com>
2019-12-16 02:31:40 +0000

    Spacing

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
         db=argv[3])
     c = db.cursor()
     query = "SELECT cities.name FROM cities" +
-    "JOIN states ON cities.state_id = states.id" +
-    "WHERE states.name=%s"
+            "JOIN states ON cities.state_id = states.id" +
+            "WHERE states.name=%s"
     c.execute(query, (input_state,))
     table = c.fetchall()
     print(", ".join([row[0] for row in table]))
