add838c7e8ce824330e90ffa2f29039b8e1dbc43
rajsudeep <sudrocks@gmail.com>
2019-12-16 04:33:49 +0000

    Refactor

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
-    session = Session()
-    row = State(name="Louisiana")
-    session.add(row)
-    session.commit()
-    print(row.id)
-    session.close()
+    new = State(name="Louisiana")
+    Session().add(row)
+    Session().commit()
+    print(new.id)
+    Session().close()
