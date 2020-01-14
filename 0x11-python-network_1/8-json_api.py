c1ae363494c2f8b7725efa60e78f1c40590370a0
rajsudeep <sudrocks@gmail.com>
2020-01-14 00:33:08 +0000

    Add task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" sends a POST request to http://0.0.0.0:5000/search_user """
-
+"""
+Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter
+as a parameter
+"""
 import requests
 from sys import argv
 
 
-if __name__ = '__main__':
+if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     if len(argv) == 2:
         q = argv[1]
