629ba1d2da70854666004e076c319b1771dedf54
rajsudeep <sudrocks@gmail.com>
2020-01-14 00:48:15 +0000

    Pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,5 +7,5 @@ from sys import argv
 
 if __name__ == "__main__":
     data = requests.get('https://api.github.com/user',
-                       auth=HTTPBasicAuth(argv[1], argv[2])).json()
+                        auth=HTTPBasicAuth(argv[1], argv[2])).json()
     print(data.get('id'))
