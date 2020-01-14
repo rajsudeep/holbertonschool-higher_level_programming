95ba1af63eef7f4227ac346cc458118ed3a7deb7
rajsudeep <sudrocks@gmail.com>
2020-01-14 00:04:54 +0000

    Add variables

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,9 +5,11 @@ import urllib.request
 import sys
 
 if __name__ == '__main__':
+    url = sys.argv[1]
+    req = request.Request(url)
     try:
-        with urllib.request.urlopen(sys.argv[1]) as f:
-            html = f.read()
+        with request.urlopen(req) as response:
+            html = response.read()
         print(html.decode('utf-8'))
     except error.HTTPError as e:
         print("Error code: {}".format(e.code))
