62a4f6266c2d72f73739b079f171cc167dd706bf
rajsudeep <sudrocks@gmail.com>
2020-01-14 00:47:21 +0000

    Add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" that takes your Github credentials (username and password) and uses API """
+
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    data = requests.get('https://api.github.com/user',
+                       auth=HTTPBasicAuth(argv[1], argv[2])).json()
+    print(data.get('id'))
