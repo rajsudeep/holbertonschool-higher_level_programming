1dd5620c239fbcf009b700cbbf2c67c2790351fe
rajsudeep <sudrocks@gmail.com>
2020-01-14 00:11:58 +0000

    Use text method

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,5 @@ import requests
 if __name__ == '__main__':
     html = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    print('\t- type: {}'.format(type(html.text)))
+    print('\t- content: {}'.format(html.text))
