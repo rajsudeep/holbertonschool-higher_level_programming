00499fb3aabc34c8d3dd68dba69d32a1c57b50eb
rajsudeep <sudrocks@gmail.com>
2020-01-14 00:38:09 +0000

    Change perms

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""
-Takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter
-as a parameter
-"""
+""" sends a POST request to http://0.0.0.0:5000/search_user with a letter """
+
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
-    if len(argv) == 2:
-        q = argv[1]
-    else:
-        q = ""
-    req = requests.post(url, data={'q': q})
+    data = {'q': ""}
+    if len(argv) > 1:
+        data['q'] = argv[1]
+    r = requests.post("http://0.0.0.0:5000/search_user", data)
     try:
-        stuff = req.json()
+        stuff = r.json()
         if stuff:
             print('[{}] {}'.format(stuff.get('id'), stuff.get('name')))
         else:
