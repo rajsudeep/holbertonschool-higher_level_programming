cc067e4c408a4c5ed2924a5ebdd29834c8516f5e
rajsudeep <sudrocks@gmail.com>
2020-02-11 22:11:03 +0000

    Add task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const swapi = `${process.argv[2]}`;
+
+request(swapi, (err, res, body) => {
+  if (err) throw new Error(err);
+  const amount = JSON.parse(body).results.reduce((i, e) => {
+    e.characters.forEach((e) => {
+      if (e.includes(18)) { i++; }
+    });
+    return i;
+  }, 0);
+  console.log(amount);
+});
