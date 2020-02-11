67d159724dd09c3c4639a0754add902c8571afcd
rajsudeep <sudrocks@gmail.com>
2020-02-11 21:56:01 +0000

    Add argument to swapi url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const swapi = 'http://swapi.co/api/films/:id';
+const swapi = `http://swapi.co/api/films/${process.argv[2]}`;
 
 request(swapi, (err, res, body) => {
   err ? console.log(err) : console.log(JSON.parse(body).title);
