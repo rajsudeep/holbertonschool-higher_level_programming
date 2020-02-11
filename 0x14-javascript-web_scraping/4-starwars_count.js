11e4d419178ff2f774f40f7e9c78fbd37c67521c
rajsudeep <sudrocks@gmail.com>
2020-02-11 22:12:12 +0000

    Condense statements

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,9 +6,7 @@ const swapi = `${process.argv[2]}`;
 request(swapi, (err, res, body) => {
   if (err) throw new Error(err);
   const amount = JSON.parse(body).results.reduce((i, e) => {
-    e.characters.forEach((e) => {
-      if (e.includes(18)) { i++; }
-    });
+    e.characters.forEach((e) => { if (e.includes(18)) { i++; } });
     return i;
   }, 0);
   console.log(amount);
